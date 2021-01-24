d6b50bd3663d06c9f37d05bdd4cb5cda9a3dccf2
Sanpreet Singh <sany26.com@gmail.com>
2021-01-24 18:01:55 +0530

    Dynamic code is implemented for adding new inputs

--- concept_list_dictionary.py
+++ concept_list_dictionary.py
@@ -54,3 +54,54 @@ of cities in the input but this is a fixed code where it will operate for only t
 final_list = [list1, list2] is not optimized because what we will do if there are a lot of cities
 we cannot manually add list there, It is a static solution. 
 """
+
+# solution 2
+cities = list()
+for lists in input_val:
+    for each_element in lists:
+        if type(each_element) == str:
+            if each_element not in cities:
+                cities.append(each_element)
+
+print("Number of cities present in the input are/is: {}".format(cities))
+
+n = len(cities)
+output_cities = [[]] * n
+temp_list = list()
+for i in range(n):
+    for lists in input_val:
+        if cities[i] in lists:
+            city_name = lists[0]
+            temp = lists[1]
+            if len(temp_list) > 0:
+                temp_list.append(temp)
+            else:
+                temp_list = [temp]
+            mean_temp = np.mean(temp_list)
+            mean_temp_round = round(mean_temp, 2)
+
+    output_cities[i] = [city_name, mean_temp_round]
+    temp_list = list()
+
+print(
+    "Result for Dynamic code for cities and their means is", output_cities
+)
+
+"""
+Discussion of the above results
+we are again using nested loop, hence time complexity is O(n**2)
+I am also using list to append the output based on number of cities \
+calculated and other list for appending all the temperatures of the \
+same city. so space complexity is more than O(1) which i think is O(n)\
+
+Let us talk about the problems being solved by this code
+1.) You can add as many as cities and their temperatures 
+ code will automatically calculate the results.
+2.) Code is dynamic in nature
+
+What to improve
+===============
+
+I am working on time and space complexity to reduced to O(n) 
+"""
+
