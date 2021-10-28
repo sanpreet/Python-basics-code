a498549d2b81e667bb32505a19ce0cfa0e8f87a7
Sanpreet Singh <sany26.com@gmail.com>
2021-10-28 18:22:43 +0530

    Update deal_with_abnormalities.py

--- data_frame/deal_with_abnormalities.py
+++ data_frame/deal_with_abnormalities.py
@@ -1,9 +1,13 @@
 import pandas as pd
 
 data_read = pd.read_csv('data.csv', encoding='utf-8')
+data_read.column1 = data_read.column1.astype(str)
 # print(data_read)
 
-# convert any string value to NaN Values 
+# convert any string value to NaN Values
+# this function applied when all the values in the dataframe are string.
+# so it is advisable to convert all the values to string before proceeding to this stage
+# numeric value with double quotes are also considered as string
 data_read.column1 = data_read.column1.str.extract('(\d+)')
 # convert the whole column to tge float type
 data_read.column1 = data_read.column1.astype(float)
