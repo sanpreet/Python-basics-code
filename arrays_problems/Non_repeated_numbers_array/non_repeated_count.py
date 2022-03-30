32d64a97ea5d2dcfbd27d2f254f70500339fd838
Sanpreet Singh <sany26.com@gmail.com>
2022-03-31 00:12:00 +0530

    Non Repeating Numbers in an array
    - used dictionary
    - loop

---
+++ arrays_problems/Non_repeated_numbers_array/non_repeated_count.py
@@ -0,0 +1,12 @@
+def countNonRepeated(arr,n):
+
+        dict_of_counts = {item:arr.count(item) for item in arr}
+        count_non_repeated = 0
+        for values in dict_of_counts.values(): 
+            if values == 1:
+                count_non_repeated += 1
+        return count_non_repeated   
+
+
+arr = [10, 20, 30, 40, 10]
+print(countNonRepeated(arr=arr, n=10))
